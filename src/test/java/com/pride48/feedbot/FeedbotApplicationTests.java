a58f3c3ff1bf2bd0265d9f73fdd615488f9a1a7d
Adam Burns <adamburns.uk@gmail.com>
2017-10-28 15:37:22 -0500

    initial commit

---
+++ src/test/java/com/pride48/feedbot/FeedbotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pride48.feedbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
