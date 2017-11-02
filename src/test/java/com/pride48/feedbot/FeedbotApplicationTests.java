4e99e630cf9b080c3ee051347a9efeafed408a67
Adam Burns <adamburns.uk@gmail.com>
2017-11-01 23:30:24 -0500

    updated

--- src/test/java/com/pride48/feedbot/FeedbotApplicationTests.java
+++ src/test/java/com/pride48/feedbot/FeedbotApplicationTests.java
@@ -1,16 +1,26 @@
 package com.pride48.feedbot;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.p48.feedbot.services.FeedBot;
+import com.rometools.rome.io.FeedException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FeedbotApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IllegalArgumentException, MalformedURLException, IOException, FeedException {
+		
+		FeedBot fb = new FeedBot();
+		
+		fb.getPodcasts("podcasts.json", "c:\\podcasts");
 	}
 
 }
